ee4cb5da80c5676e5f3534419e7b72291f4ec788
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-14 01:11:01 -0300

    commit01

--- BibliotecaVirtual_CSharp/ClassePrincipal.cs
+++ BibliotecaVirtual_CSharp/ClassePrincipal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BibliotecaVirtual
 {
@@ -9,8 +10,9 @@ namespace BibliotecaVirtual
         {
             List<Livros> livros = new List<Livros>();
             char verificador;
-            bool check = true;
-            bool teste = true;
+            char variavel;
+            bool boole = true;
+            bool teste = true;            
 
             do
             {
@@ -41,22 +43,151 @@ namespace BibliotecaVirtual
 
 
                     case '2':
-                        
-                            /*for (int i = 0; i < livros.Count; i++)
-                            {
-                            Console.WriteLine(i + " - " + livros(i).GetTitulo() + 
-                                "\n_________________________\n");
-                            }*/
-                        
-                        break;
-                        
 
-                    
+                            if (livros.Count() > 0)
+                            {
+                                for (int i = 0; i < livros.Count(); i++)
+                                {
+                                    Console.WriteLine(i + " - " + livros[i].GetTitulo() + "\n_________________________\n");
+                                }
+
+                                int cont = 0;
+
+                            do
+                            {
+                                try
+                                {
+                                    Console.WriteLine("Insira o código do livro a ser deletado: ");
+                                    if (cont != 0) 
+                                    {
+                                        Console.ReadLine();
+                                    }
+                                        int check = int.Parse(Console.ReadLine());
+                                        Livros.SetTotalDeTitulos(totalDeTitulos - 1);
+                                        Livros.SetTotalDeExemplares(totalDeExemplares - livros.Count(check).NumeroExemplares());
+                                        livros.Remove(check);
+
+                                        Console.WriteLine("Livri excluido com êxito!");
+                                        Livros.enterToContinue();
+
+                                        teste = false;
+                                    
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Caracter inválido!");
+                                    teste = true;
+                                    cont++;
+                                }
+
+                            } while (teste);
+                            } 
+                            else
+                            {
+                                Console.WriteLine("Tente cadastrar algo antes de exluir!");
+                                Livros.enterToContinue();
+                            }
+
+                        break;
+
+                    case '3':
+                        if (livros.Count() > 0)
+                        {
+                            for (int i = 0; i < livros.Count(); i++)
+                            {
+                                Console.WriteLine(i + " - " + livros[i].GetTitulo() + "\n_________________________\n");
+                            }
+
+                            int cont = 0;
+
+                            do
+                            {
+                                try
+                                {
+                                    Console.WriteLine("Insira o codigo do livro a ser editado: ");
+                                    if (cont != 0)
+                                    {
+                                        Console.ReadLine();
+                                    }
+                                    int check = int.Parse(Console.ReadLine());
+                                    Livros.SetTotalDeTitulos(totalDeTitulos - 1);
+                                    Livros.SetTotalDeExemplares(totalDeExemplares - livros.Count(check).NumeroExemplares());
+                                    livros.Remove(check);
+
+                                    Livros b = new Livros();
+
+                                    b.CadastrarLivros();
+
+                                    livros.Add(b);
+
+                                    Console.WriteLine("Livros editado com êxito: ");
+                                    Livros.enterToContinue();
+
+                                    teste = false;
+                                }
+                                catch (Exception)
+                                {
+                                    Console.WriteLine("Caracter inválido!");
+                                    teste = true;
+                                    cont++;
+                                }
+                            } while (teste);
+
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Tente cadastrar algo antes de editar!");
+                            Livros.enterToContinue();
+                        }
+
+                        break;
+
+                    case '4':
+                        if (livros.Count() > 0)
+                        {
+                            
+                            bool booleano = true;
+                        }
+                        do
+                        {
+                            Console.WriteLine("\n\tDeseja pesquisar livros pelo: "+
+                                        "\n\t___________________" +
+                                        "\n\t|\t1 - TÍTULO\t\t|" +
+                                        "\n\t|\t2 - AUTOR\t\t|" +
+                                        "\n\t|t3 - EDITORA\t\t|" +
+                                        "\n\t|t4 - SAIR\t\t|" +
+                                        "\n\t_____________________");
+                            var varDigitado = Console.ReadLine();
+                            variavel = varDigitado[0];
+
+                            switch (variavel)
+                            {
+                                case '1':
+                                    Livros buscaTitulo = new Livros();
+                                    Console.WriteLine("Digite o título do livro a ser procurado: ");
+                                    Console.ReadLine();
+                                    buscaTitulo.SetTitulo(Console.ReadLine());
+                                    for (int j = 0; j < Livros.cont; j++)
+                                    {
+                                        if (buscaTitulo.GetTitulo().Equals(livros.Count(j).GetTitulo()))
+                                        {
+                                            Console.WriteLine();
+                                            Livros.enterToContinue();
+                                        }
+                                    }
+
+                                    break;
+                            }
+
+                        } while (true);
+
+                        break;
+
                 }
 
-            } while (true);
-            
-          
+            } while (boole);
+
+            Console.WriteLine("Sistema finalizado!");
         }
     }
 }
