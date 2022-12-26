900895f3b4e176686d7e96731d75580e7bff78d9
Wesley Jaguaracy <79489592+WesleyJaguaracy@users.noreply.github.com>
2022-12-25 22:39:05 -0300

    Create Converter Pdf

---
+++ Caminho_Pdf/ConverterPdf.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace CaminhoFisicoPdf
+{
+    class ConverterPdf
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExtraiTextoDePdf(@"C:\Users\Wesle\source\repos\CaminhoFisicoPdf\TabelaNomes.pdf"));
+            Console.ReadKey();
+            //Console.WriteLine("Qual o caminho fisico do arquivo?");
+            //Console.ReadLine();
+        }
+
+        static string ExtraiTextoDePdf(string nomeDoArquivo)
+        {
+            string result = null;
+            PdfReader pdfReader = new PdfReader(nomeDoArquivo);
+            PdfDocument pdfDoc = new PdfDocument(pdfReader);
+            for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string conteudo = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+                result += conteudo;
+            }
+            pdfDoc.Close();
+            pdfReader.Close();
+            return result;
+        }
+    }
+}
